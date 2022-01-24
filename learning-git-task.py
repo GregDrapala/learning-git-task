ac00c7c0fd973c0b1400eb2229596071d2554e22
Greg Drapala <drapalagrzegorz@gmail.com>
2022-01-24 12:15:02 +0000

    add loop

--- learning-git-task.py
+++ learning-git-task.py
@@ -2,3 +2,5 @@ shopping_dict = {
     "piekarnia": ["chleb", "paczek", "bulki"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+for sklep, rzeczy in shopping_dict.items():
+  print("ide do " + str(sklep.) + " kupuje tam" + str(rzeczy))
