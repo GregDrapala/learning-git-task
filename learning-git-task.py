2c3d736aa888aa36fcbabd66d21fc0bd43e5b462
Greg Drapala <drapalagrzegorz@gmail.com>
2022-01-24 12:19:30 +0000

    add len

--- learning-git-task.py
+++ learning-git-task.py
@@ -4,3 +4,5 @@ shopping_dict = {
 }
 for sklep, rzeczy in shopping_dict.items():
   print("ide do " + str(sklep.title()) + " kupuje tam" + str(rzeczy))
+  x = len(shopping_dict["piekarnia"]) + len(shopping_dict["warzywniak"])
+print("w sumie kupuje " + str(x) + " produktow" )
